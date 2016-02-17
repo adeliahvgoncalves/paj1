171a7870007718da6c70c42ad01264198db2086b
Adélia Gonçalves <adeliahvgoncalves@gmail.com>
2016-02-17 20:52:28 +0000

    hxtml1

--- src/main/java/pt/uc/dei/aor/paj/examples/hello1/HelloBackingBean.java
+++ src/main/java/pt/uc/dei/aor/paj/examples/hello1/HelloBackingBean.java
@@ -1,13 +1,24 @@
 package pt.uc.dei.aor.paj.examples.hello1;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
-@RequestScoped
-public class HelloBackingBean {
+@SessionScoped
+public class HelloBackingBean implements Serializable {
 
 	private String name;
+
+	public void logout() throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.invalidateSession();
+		ec.redirect(ec.getRequestContextPath() + "/frontpage.xhtml");
+	}
 
 	public void setName(String name) {
 		this.name = name;
